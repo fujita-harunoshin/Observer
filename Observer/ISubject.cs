e1bd427b7f1b20e80f22b7c37aac48186ee03f66
Harunoshin Fujita <fujita.harunoshin@gmail.com>
2024-06-09 07:43:25 +0900

    プロジェクト ファイルを追加します。

---
+++ Observer/ISubject.cs
@@ -0,0 +1,10 @@
+﻿namespace Observer;
+
+internal interface ISubject
+{
+    void Attach(IObserver observer);
+
+    void Detach(IObserver observer);
+
+    void Notify();
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Observer;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var subject = new Subject();
+        var observerA = new ConcreteObserverA();
+        subject.Attach(observerA);
+
+        var observerB = new ConcreteObserverB();
+        subject.Attach(observerB);
+
+        subject.SomeBusinessLogic();
+        subject.SomeBusinessLogic();
+
+        subject.Detach(observerB);
+
+        subject.SomeBusinessLogic();
+    }
+}
